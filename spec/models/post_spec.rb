66b5a1c6d227fb9e27757db5ba992abbb3d88e69
iman <imanqhavami.80@gmail.com>
2024-01-26 04:19:27 +0330

    created rspec files for project

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    post = Post.new(title: "Test Post", body: "This is a test post.", liked: false)
+    expect(post).to be_valid
+  end
+
+  it "is not valid without a title" do
+    post = Post.new(body: "This is a test post.")
+    expect(post).not_to be_valid
+  end
+
+  it "is not valid without body" do
+    post = Post.new(title: "Test Post")
+    expect(post).not_to be_valid
+  end
 end
